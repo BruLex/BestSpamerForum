2423812518fe14a78912f11a0fa3a93ab454fd04
Anton Trukhan <deko71dekobraz@gmail.com>
2020-03-07 10:56:03 +0200

    Merge apps

---
+++ web-app/src/app/my-posts/my-posts.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './my-posts.component.html',
+  styleUrls: ['./my-posts.component.scss']
+})
+export class MyPostsComponent {
+
+}
